f03807870c5cb2643d01e99b36b71109a132cd20
GManzke <gabrielmzke@gmail.com>
2023-10-31 09:59:36 -0300

    build: Bump apps version to 1.0.0

--- test/core/data/data_sources/user_location_data_source_test.dart
+++ test/core/data/data_sources/user_location_data_source_test.dart
@@ -14,26 +14,58 @@ void main() {
     userLocationDataSourceImpl = UserLocationDataSourceImpl(locationService);
   });
 
-  test('Should return the current location', () async {
-    when(() => locationService.getLocation()).thenAnswer(
-      (_) async => (latitude: 1.0, longitude: 2.0),
-    );
+  group('getLocation - ', () {
+    test('Should return the current location', () async {
+      when(() => locationService.getLocation()).thenAnswer(
+        (_) async => (latitude: 1.0, longitude: 2.0),
+      );
 
-    final (latitude: lat, longitude: long) =
-        await userLocationDataSourceImpl.getLocation();
+      final (latitude: lat, longitude: long) =
+          await userLocationDataSourceImpl.getLocation();
 
-    expect(lat, 1.0);
-    expect(long, 2.0);
-    verify(() => locationService.getLocation()).called(1);
+      expect(lat, 1.0);
+      expect(long, 2.0);
+      verify(() => locationService.getLocation()).called(1);
+    });
+
+    test('Should throw an exception if the location service fails', () async {
+      when(() => locationService.getLocation()).thenThrow(Exception());
+
+      expect(
+        () async => await userLocationDataSourceImpl.getLocation(),
+        throwsA(isA<Exception>()),
+      );
+      verify(() => locationService.getLocation()).called(1);
+    });
   });
 
-  test('Should throw an exception if the location service fails', () async {
-    when(() => locationService.getLocation()).thenThrow(Exception());
+  group('getDistanceInMeters - ', () {
+    const origin = (latitude: 1.0, longitude: 2.0);
+    const destination = (latitude: 3.0, longitude: 4.0);
 
-    expect(
-      () async => await userLocationDataSourceImpl.getLocation(),
-      throwsA(isA<Exception>()),
-    );
-    verify(() => locationService.getLocation()).called(1);
+    test('Should return the distance in meters', () {
+      when(() => locationService.getDistanceInMeters(origin, destination))
+          .thenReturn(1000);
+
+      final distance = userLocationDataSourceImpl.getDistanceInMeters(
+        origin,
+        destination,
+      );
+
+      expect(distance, 1000);
+    });
+
+    test('Should throw an exception if the location service fails', () async {
+      when(() => locationService.getDistanceInMeters(origin, destination))
+          .thenThrow(Exception());
+
+      expect(
+        () => userLocationDataSourceImpl.getDistanceInMeters(
+          origin,
+          destination,
+        ),
+        throwsA(isA<Exception>()),
+      );
+    });
   });
 }
